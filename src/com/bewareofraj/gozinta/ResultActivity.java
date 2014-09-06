00485f157ed9e5fe4c681e51758cb640c4ee734a
Raj Ramsaroop <raj@bewareofraj.com>
2014-09-06 03:40:15 -0400

    add UI variables for result screen

--- src/com/bewareofraj/gozinta/ResultActivity.java
+++ src/com/bewareofraj/gozinta/ResultActivity.java
@@ -1,7 +1,9 @@
 package com.bewareofraj.gozinta;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ResultActivity extends Activity {
 	
@@ -18,22 +20,35 @@ public class ResultActivity extends Activity {
 	private float finalTotal;
 	private int numberOfPeople;
 	private float costPerPerson;
+	
+	private TextView txtBillTotal;
+	private TextView txtTipAmount;
+	private TextView lblTipAmount;
+	private TextView txtFinalTotal;
+	private TextView lblCostPerPerson;
+	private TextView txtCostPerPerson;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_result);
 		
-		getValuesFromBundle(savedInstanceState);
+		Intent intent = getIntent();
+		getValuesFromBundle(intent);
+		setTextViewValues();
 		
 	}
 
-	private void getValuesFromBundle(Bundle savedInstanceState) {
-		totalBill = savedInstanceState.getFloat(EXTRA_TOTAL_KEY);
-		tipAmount = savedInstanceState.getFloat(EXTRA_TIP_AMOUNT_KEY);
-		tipPercentage = savedInstanceState.getFloat(EXTRA_TIP_PERCENTAGE_KEY);
-		finalTotal = savedInstanceState.getFloat(EXTRA_FINAL_TOTAL_KEY);
-		numberOfPeople = savedInstanceState.getInt(EXTRA_NUMBER_OF_PEOPLE_KEY);
-		costPerPerson = savedInstanceState.getFloat(EXTRA_COST_PER_PERSON_KEY);
+	private void setTextViewValues() {
+		
+	}
+
+	private void getValuesFromBundle(Intent intent) {
+		totalBill = intent.getFloatExtra(EXTRA_FINAL_TOTAL_KEY, 0f);
+		tipAmount = intent.getFloatExtra(EXTRA_TIP_AMOUNT_KEY, 0f);
+		tipPercentage = intent.getFloatExtra(EXTRA_TIP_PERCENTAGE_KEY, 0f);
+		finalTotal = intent.getFloatExtra(EXTRA_FINAL_TOTAL_KEY, 0f);
+		numberOfPeople = intent.getIntExtra(EXTRA_NUMBER_OF_PEOPLE_KEY, 0);
+		costPerPerson = intent.getFloatExtra(EXTRA_COST_PER_PERSON_KEY, 0f);
 	}
 }
