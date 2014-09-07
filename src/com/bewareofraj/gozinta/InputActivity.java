07deab9271f289928f938058684f259524b52667
Raj Ramsaroop <raj@bewareofraj.com>
2014-09-06 20:11:55 -0400

    add easter egg activity

--- src/com/bewareofraj/gozinta/InputActivity.java
+++ src/com/bewareofraj/gozinta/InputActivity.java
@@ -3,6 +3,9 @@ package com.bewareofraj.gozinta;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -26,6 +29,25 @@ public class InputActivity extends Activity {
 
 		initializeGui();
 	}
+	
+	@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_input, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+	
+	@Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case R.id.japan:
+        		Intent japanIntent = new Intent(InputActivity.this, JapanActivity.class);
+        		startActivity(japanIntent);
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
+        }
+    }
 
 	private void initializeGui() {
 		// initialize UI
---
+++ src/com/bewareofraj/gozinta/JapanActivity.java
@@ -0,0 +1,13 @@
+package com.bewareofraj.gozinta;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class JapanActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_japan);
+	}
+}
